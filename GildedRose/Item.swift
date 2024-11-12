a72cdb879bdc770841dc76d43247265fedb1064d
andy <andy.bezaire@gmail.com>
2024-11-12 07:33:35 +0200

    Remove unneded implementation for `Equatable`

--- GildedRose/Item.swift
+++ GildedRose/Item.swift
@@ -1,11 +1,5 @@
 public struct Item: Equatable {
-    
     var name: String
     var sellIn: Int
     var quality: Int
-    
-}
-
-public func ==(lhs: Item, rhs: Item) -> Bool {
-    return lhs.name == rhs.name && lhs.sellIn == rhs.sellIn && lhs.quality == rhs.quality
 }
